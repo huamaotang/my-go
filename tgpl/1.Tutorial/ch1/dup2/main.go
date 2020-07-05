2ced918d7df7fed266cc541a5911f1e8661785f0
汤华茂 <tanghuamao@noahgroup.com>
2020-07-05 22:21:18 +0800

    finding duplicate lines

--- tgpl/1.Tutorial/ch1/dup2/main.go
+++ tgpl/1.Tutorial/ch1/dup2/main.go
@@ -15,9 +15,10 @@ import (
 )
 
 func main() {
-	counts := make(map[string]int)
+	file := make(map[string]map[string]int)
 	files := os.Args[1:]
 	if len(files) == 0 {
+		counts := make(map[string]int)
 		countLines(os.Stdin, counts)
 	} else {
 		for _, arg := range files {
@@ -26,13 +27,17 @@ func main() {
 				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
 				continue
 			}
+			counts := make(map[string]int)
 			countLines(f, counts)
+			file[arg] = counts
 			f.Close()
 		}
 	}
-	for line, n := range counts {
-		if n > 1 {
-			fmt.Printf("%d\t%s\n", n, line)
+	for k, counts := range file {
+		for line, n := range counts {
+			if n > 1 {
+				fmt.Printf("%s: %d\t%s\n", k, n, line)
+			}
 		}
 	}
 }
